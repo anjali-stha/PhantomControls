9dc068816b65a7218b5e8db94c2360a71af66793
Husna Fatima Husna <201167406+Husna72@users.noreply.github.com>
2025-03-02 17:43:52 -0600

    Update phantomControls.py
    Shooting Accuracy: Now detects "OK" sign for shooting more precisely using dynamic thresholds.
    Less Glitchy: Fixed false triggers and improved gesture reliability.

--- phantomControls.py
+++ phantomControls.py
@@ -22,14 +22,14 @@ spike_controls = False
 stop = False
 
 def reset_controls():
-    pydirectinput.keyUp(5)
+    pydirectinput.keyUp("5")
     pydirectinput.keyUp("space")
     pydirectinput.keyUp("w")
     pydirectinput.keyUp("s")
     pydirectinput.keyUp("a")
     pydirectinput.keyUp("d")
     pydirectinput.keyUp("r")
-    pydirectinput.keyUp(4)
+    pydirectinput.keyUp("4")
 
 while True:
     success, img = cap.read()
@@ -44,8 +44,6 @@ while True:
     right_wrist_y = None
     left_wrist_y = None
     head_y = None
-    right_hand_open = False
-    left_hand_open = False
     left_shoulder_y = None
     right_shoulder_y = None
     right_elbow_y = None
@@ -53,29 +51,28 @@ while True:
 
     if results_pose.pose_landmarks:
         mpDraw.draw_landmarks(img, results_pose.pose_landmarks, mpPose.POSE_CONNECTIONS)
-        h, w, c = img.shape 
+        h, w, c = img.shape  
 
         for id, lm in enumerate(results_pose.pose_landmarks.landmark):
             cx, cy = int(lm.x * w), int(lm.y * h)
 
             if id == 16:  
                 right_wrist_y = cy
-            elif id == 15:  
+            elif id == 15: 
                 left_wrist_y = cy
-            elif id == 0: 
+            elif id == 0:  
                 head_y = cy
-            elif id == 11: 
+            elif id == 11:  
                 left_shoulder_y = cy
-            elif id == 12:  
+            elif id == 12: 
                 right_shoulder_y = cy
-            if id == 14: 
+            elif id == 14: 
                 right_elbow_y = cy
-            elif id == 13:
+            elif id == 13: 
                 left_elbow_y = cy
 
             cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
-
-    
+            
     if results_hands.multi_hand_landmarks:
         for handLms in results_hands.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
@@ -91,10 +88,10 @@ while True:
             pinky_tip_y = landmarks[20].y * h
 
             hand_side = "right" if landmarks[0].x > 0.5 else "left"
-
+            
             is_hand_open = (
-                index_tip_y < wrist_y and middle_tip_y < wrist_y and
-                ring_tip_y < wrist_y and pinky_tip_y < wrist_y
+                    index_tip_y < wrist_y and middle_tip_y < wrist_y and
+                    ring_tip_y < wrist_y and pinky_tip_y < wrist_y
             )
 
             if thumb_tip_y < wrist_y and index_tip_y < wrist_y and is_hand_open:
@@ -105,8 +102,7 @@ while True:
                 if forward:
                     pydirectinput.keyUp("w")
                     forward = False
-
-            if thumb_tip_y > wrist_y and index_tip_y > wrist_y:
+             if thumb_tip_y > wrist_y and index_tip_y > wrist_y:
                 if not backward:
                     pydirectinput.keyDown("s")
                     backward = True
@@ -126,22 +122,44 @@ while True:
                     continue
                 stop = False
 
+
+            if hand_side == "right":
+                right_hand_open = is_hand_open
+            else:
+                left_hand_open = is_hand_open
+                if right_hand_open and left_hand_open:
+                    if not stop:
+                        reset_controls()
+                        stop = True
+                    continue
+                stop = False
+
+           
             thumb_tip_x = landmarks[4].x * w  
-            thumb_tip_y = landmarks[4].y * h 
+            thumb_tip_y = landmarks[4].y * h  
             index_tip_x = landmarks[8].x * w  
             index_tip_y = landmarks[8].y * h  
+            middle_tip_y = landmarks[12].y * h  
+            ring_tip_y = landmarks[16].y * h  
+            pinky_tip_y = landmarks[20].y * h  
 
-            if abs(thumb_tip_x - index_tip_x) < 20 and abs(thumb_tip_y - index_tip_y) < 20:
+            thumb_index_distance = ((thumb_tip_x - index_tip_x) ** 2 + (thumb_tip_y - index_tip_y) ** 2) ** 0.5  
+
+            hand_width = abs(landmarks[0].x - landmarks[9].x) * w  
+            shoot_threshold = hand_width * 0.25  
+
+            if thumb_index_distance < shoot_threshold and (middle_tip_y > index_tip_y and ring_tip_y > index_tip_y):
                 if not shoot:
                     pydirectinput.keyDown("5")  
+                    shoot = True
             else:
                 if shoot:
                     pydirectinput.keyUp("5")
                     shoot = False
 
-    
+  
     if left_shoulder_y is not None and right_shoulder_y is not None:
-        if left_shoulder_y > right_shoulder_y + 10:  # Tilt Left
+        if left_shoulder_y > right_shoulder_y + 10:  
             pydirectinput.keyDown("a")
             moving_left = True
         else:
@@ -149,9 +167,8 @@ while True:
                 pydirectinput.keyUp("a")
                 moving_left = False
 
-    
     if left_shoulder_y is not None and right_shoulder_y is not None:
-        if right_shoulder_y > left_shoulder_y + 10:  # Tilt Right
+        if right_shoulder_y > left_shoulder_y + 10:  
             pydirectinput.keyDown("d")
             moving_right = True
         else:
@@ -159,9 +176,9 @@ while True:
                 pydirectinput.keyUp("d")
                 moving_right = False
 
-    
+   
     if left_elbow_y is not None and left_shoulder_y is not None:
-        if left_elbow_y < left_shoulder_y:  # Elbow above shoulder
+        if left_elbow_y < left_shoulder_y:  
             if not reload:
                 pydirectinput.keyDown("r")
                 reload = True
@@ -170,31 +187,12 @@ while True:
                 pydirectinput.keyUp("r")
                 reload = False
 
-    if results_hands.multi_hand_landmarks:
-        for handLms in results_hands.multi_hand_landmarks:
-            landmarks = handLms.landmark
-
-            left_index_x = landmarks[8].x * w  
-            left_index_y = landmarks[8].y * h  
-
-            right_elbow_x = results_pose.pose_landmarks.landmark[14].x * w  
-            right_elbow_y = results_pose.pose_landmarks.landmark[14].y * h  
-
-            if abs(left_index_x - right_elbow_x) < 20 and abs(left_index_y - right_elbow_y) < 20:
-                if not spike_controls:
-                    pydirectinput.keyDown("4")  
-                    spike_controls = True
-            else:
-                if spike_controls:
-                    pydirectinput.keyUp("4")
-                    spike_controls = False
-
     if head_y is not None and right_wrist_y is not None and left_wrist_y is not None:
         if right_wrist_y < head_y and left_wrist_y < head_y:  
             if not jump:
                 pydirectinput.keyDown("space")
                 jump = True
-        else:  
+        else:
             if jump:
                 pydirectinput.keyUp("space")
                 jump = False
